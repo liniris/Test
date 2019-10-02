db0789e7122882eefac4df2c5ea019dcc834fe9e
Lin Iris <iris861219@gmail.com>
2019-10-02 16:00:15 +0800

    hello world

---
+++ vue/test.js
@@ -0,0 +1,9 @@
+// document.getElementById("app").innerText='hello world';
+// var a = 1;
+
+var app = new Vue({
+    el:'#app',   //element，綁住app
+    data:{
+        message:'helloworld'
+    }
+});
